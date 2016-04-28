01e7abf28ab40dd44da589bc87fe50af25654d6a
Iwan <zobenko96@mail.ru>
2016-04-28 23:15:20 +0300

    fixed roomService

--- src/main/java/hotels/business/room/repository/RoomRepository.java
+++ src/main/java/hotels/business/room/repository/RoomRepository.java
@@ -14,22 +14,55 @@ import java.util.Optional;
  */
 public interface RoomRepository extends JpaRepository<Room, Long> {
 
-    Optional<Room> findOneById( Long id );
+    Optional<Room> findOneById(Long id);
 
-    @Query( "SELECT r FROM Room r " +
+    @Query("SELECT r FROM Room r " +
             "WHERE r.roomType = :roomType " +
-            "AND LOWER(r.bedType) LIKE LOWER(:bedType) " +
-            "AND LOWER(r.breakfast) LIKE LOWER(:breakfast)" )
-    Page<Room> findAllByRoomTypeAndBedTypeAndBreakfast( Pageable pageable,
-                                                        @Param( "roomType" ) String roomType,
-                                                        @Param( "bedType" ) String bedType,
-                                                        @Param( "breakfast" ) String breakfast );
+            "AND r.bedType = :bedType " +
+            "AND r.breakfast = :breakfast")
+    Page<Room> findAllByRoomTypeAndBedTypeAndBreakfast(Pageable pageable,
+                                                       @Param("roomType") Room.RoomType roomType,
+                                                       @Param("bedType") Room.BedType bedType,
+                                                       @Param("breakfast") Room.Breakfast breakfast);
 
-    @Query( "SELECT r FROM Room r " +
-            "WHERE LOWER(r.bedType) LIKE LOWER(:bedType) " +
-            "AND LOWER(r.breakfast) LIKE LOWER(:breakfast)" )
-    Page<Room> findAllByBedTypeAndBreakfast( Pageable pageable,
-                                             @Param( "bedType" ) String bedType,
-                                             @Param( "breakfast" ) String breakfast );
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.bedType = :bedType " +
+            "AND r.breakfast = :breakfast")
+    Page<Room> findAllByBedTypeAndBreakfast(Pageable pageable,
+                                            @Param("bedType") Room.BedType bedType,
+                                            @Param("breakfast") Room.Breakfast breakfast);
+
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.roomType = :roomType " +
+            "AND r.bedType = :bedType")
+    Page<Room> findAllByRoomTypeAndBedType(Pageable pageable,
+                                           @Param("roomType") Room.RoomType roomType,
+                                           @Param("bedType") Room.BedType bedType);
+
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.roomType = :roomType " +
+            "AND r.breakfast = :breakfast")
+    Page<Room> findAllByRoomTypeAndBreakfast(Pageable pageable,
+                                             @Param("roomType") Room.RoomType roomType,
+                                             @Param("breakfast") Room.Breakfast breakfast);
+
+    @Query("SELECT r FROM Room r " +
+            "WHERE r.roomType = :roomType")
+    Page<Room> findAllByRoomType(Pageable pageable,
+                                 @Param("roomType") Room.RoomType roomType);
+
+    @Query("SELECT r FROM Room r " +
+            "where r.bedType = :bedType")
+    Page<Room> findAllByBedType(Pageable pageable,
+                                @Param("bedType") Room.BedType bedType);
+
+    @Query("SELECT r FROM Room r " +
+            "where r.breakfast = :breakfast")
+    Page<Room> findAllByBreakfast(Pageable pageable,
+                                  @Param("breakfast") Room.Breakfast breakfast);
+
+    @Query("SELECT r FROM Room r")
+    Page<Room> getAll(Pageable pageable);
+
 
 }
